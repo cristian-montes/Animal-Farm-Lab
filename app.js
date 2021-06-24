c5d2c921bff4278ba1735c4bae5a5b7b01988999
Cristian Montes <cmontesmujica@gmail.com>
2021-06-24 08:08:46 -0700

    keydown effect added

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 
-// CAT
-const catSound = document.getElementById('cat-sound');
-const cat = document.getElementById('cat');
- 
-cat.addEventListener('click', () => {
-    catSound.play();
+// ANIMAL NOISES WITH KEYS
+document.addEventListener('keydown', function(e){
+    if (e.key === 'H'){
+        document.getElementById('horse-sound').play();
+    } if (e.key === 'D'){
+        document.getElementById('dog-sound').play();
+    } if (e.key === 'C'){
+        document.getElementById('cat-sound').play();
+    }
 });
+
+
+
+
+// CAT
+// const catSound = document.getElementById('cat-sound');
+// const cat = document.getElementById('cat');
+ 
+// cat.addEventListener('click', () => {
+//     catSound.play();
+// });
 
 // DOG
-const dogSound = document.getElementById('dog-sound');
-const dog = document.getElementById('dog');
+// const dogSound = document.getElementById('dog-sound');
+// const dog = document.getElementById('dog');
  
-dog.addEventListener('click', () => {
-    dogSound.play();
-});
+// dog.addEventListener('click', () => {
+//     dogSound.play();
+// });
 
 // HORSE
-const horseSound = document.getElementById('horse-sound');
-const horse = document.getElementById('horse');
+//const horseSound = document.getElementById('horse-sound');
+// const horse = document.getElementById('horse');
  
-horse.addEventListener('click', () => {
-    horseSound.play();
-});
+// horse.addEventListener('click', () => {
+//     horseSound.play();
+// })
