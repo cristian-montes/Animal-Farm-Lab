5f322431c130e50f75d9abdeb9a68ae71f54516c
Cristian Montes <cmontesmujica@gmail.com>
2021-06-24 21:16:35 -0700

    added click sounds to the imgs

--- app.js
+++ app.js
@@ -12,27 +12,28 @@ document.addEventListener('keydown', function(e){
 
 
 
+//CLICK ANIMAL NOISES
 
 // CAT
-// const catSound = document.getElementById('cat-sound');
-// const cat = document.getElementById('cat');
+const catSound = document.getElementById('cat-sound');
+const cat = document.getElementById('cat');
  
-// cat.addEventListener('click', () => {
-//     catSound.play();
-// });
+cat.addEventListener('click', () => {
+    catSound.play();
+});
 
 // DOG
-// const dogSound = document.getElementById('dog-sound');
-// const dog = document.getElementById('dog');
+const dogSound = document.getElementById('dog-sound');
+const dog = document.getElementById('dog');
  
-// dog.addEventListener('click', () => {
-//     dogSound.play();
-// });
+dog.addEventListener('click', () => {
+    dogSound.play();
+});
 
 // HORSE
-//const horseSound = document.getElementById('horse-sound');
-// const horse = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
+const horse = document.getElementById('horse');
  
-// horse.addEventListener('click', () => {
-//     horseSound.play();
-// })
+horse.addEventListener('click', () => {
+    horseSound.play();
+});
